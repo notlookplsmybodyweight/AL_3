b39677f0ad95d5fefa5695cd95788bddc5c23922
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-05-14 13:11:36 +0900

    02-01確認課題
    更新

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -1,1 +1,18 @@
 #pragma once
+#include "Model.h"
+#include "WorldTransform.h"
+#include"ViewProjection.h"
+class Player {
+public:
+	Player();
+	~Player();
+	void initialize(Model*model_,uint32_t textuerHanndle,ViewProjection * viewprojection);
+	void Update();
+	void Draw(worldTransform_,*viewProjection,textureHandle_);
+
+private:
+	WorldTransform worldTransform_;
+	Model* model_ = nullptr;
+	uint32_t textureHandle_ = 0u;
+
+}
