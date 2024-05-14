976c207f51d4f8588b71ce3d8c167d8016f84195
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-05-14 13:05:01 +0900

    02-01確認課題
    02-01の課題の途中経過の保存

---
+++ DirectXGame/Player.cpp
@@ -0,0 +1,28 @@
+#include"Player.h"
+
+void Player::initialize() {
+assert(model_);
+
+
+
+
+}
+
+Player::Player() {}
+
+Player::~Player() {}
+
+void Player::Update() {
+
+
+}
+
+void Player::Draw() {
+
+
+
+
+	model_->Draw();
+
+
+}
