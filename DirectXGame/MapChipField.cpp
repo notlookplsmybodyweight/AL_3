05096ef99deaa463c13c53cbdfebca88d3661a02
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-06-04 12:57:55 +0900

    スライド16枚目まで進めた
    エラーはないが警告が何個か出た

---
+++ DirectXGame/MapChipField.cpp
@@ -0,0 +1,36 @@
+﻿#include "MapChipField.h"
+#include <map>
+#include <fsstream>
+#include<sstream>
+namespace {
+std::map<std::string, MapChipType> MapChipTable = {
+    {"0", MapChipType::kblank},
+    {"1", MapChipType::kBlock}
+    .
+};
+}
+
+void ResetMapChipField {
+	mapChipData_.data_.clear();
+	mapChipData_.data_.resize(kNumBlockVirtical);
+    
+    
+    for (std::vector<MapChipType>& mapChipDataLine : mapChipData_.data) {
+	
+    mapChipDataLine.resize(kNumBlockHorizontal);
+
+	}
+
+
+}
+void LoadMapChipCsv { 
+    ResetMapChipData();
+    
+    std::ifstream file;
+	file.open(/map.csv);
+	assert(file.is_open);
+	std::stream mapChipCsv;
+	mapChipCsv;
+	mapChipCsv << file.rdbuf;
+	file.close();
+    }
---
+++ DirectXGame/MapChipField.h
@@ -0,0 +1,24 @@
+﻿class MapChipField{
+
+	//１ブロックのサイズ
+	static inline const float kBlockWidth = 1.0f;
+	static inline const float kBlockHeight = 1.0f;
+	//ブロックの個数
+	static inline const uint32_t kNumBlockVirtical = 20;
+	static inline const uint32_t kNumBlockHorizontal = 100;
+	MapChipData mapChipData_;
+
+} enum Class MapChipType { 
+	kBlank;
+	kBlock;
+	};
+struct MapChipData {
+	std::vector<std::vector<MapChipType>> data;
+
+
+
+
+
+	void ResetMapChipData();
+	void LoadMapChipCsv(const std::string& filepath);
+};
