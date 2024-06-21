92504aa411b5417d9acfc2f525393ec3685aa0f1
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-06-21 11:46:47 +0900

    授業で説明受けたところを進めた

--- DirectXGame/MyMath.h
+++ DirectXGame/MyMath.h
@@ -9,3 +9,7 @@ Matrix4x4 MakeRotateYMatrix(float radian);
 Matrix4x4 MakeRotateZMatrix(float radian);
 
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+//スライド23、24座標補間（線形補間）
+//{lerp(X,X2,t){return{(1.0f-time)*X+time*X2;}
+// Lerp関数Vector3 Lerp(Vector3& v1,Vector3& v2,float t){
+// return Vector3(Lerp(V1.X,V2.X,t),Vector3(Lerp(V1.Y,V2.Y,t),Vector3(Lerp(V1.X,V2.X,t));
